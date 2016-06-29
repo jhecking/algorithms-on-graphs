8b5a228ea3cc2096fedd12ae8e0bfd256adecb51
Jan Hecking <jhecking@netgaroo.com>
2016-06-30 00:45:18 +0800

    impl. GraphReader struct to generate adjacency list

--- src/main.rs
+++ src/main.rs
@@ -3,55 +3,45 @@ use std::io::BufReader;
 use std::io::BufRead;
 use std::collections::HashMap;
 
-#[derive(Hash, Eq, PartialEq, Debug)]
-struct Vertex {
-    name: String,
+struct GraphReader<T: BufRead> {
+    buf: T,
+    vertices: usize,
+    edges: usize,
 }
 
-impl Vertex {
-    fn new(name: &str) -> Vertex {
-        Vertex { name: name.to_string() }
+impl<T: BufRead> GraphReader<T> {
+    fn new(mut reader: T) -> GraphReader<T> {
+        let mut buffer = String::new();
+        reader.read_line(&mut buffer).unwrap();
+        let dim: Vec<usize> = buffer.split_whitespace().map(|s| s.parse().unwrap()).collect();
+        GraphReader { buf: reader, vertices: dim[0], edges: dim[1] }
     }
-}
 
-struct Edge {
-    a: Vertex,
-    b: Vertex,
-}
-
-impl Edge {
-    fn new(a: &str, b: &str) -> Edge {
-        Edge { a: Vertex::new(a), b: Vertex::new(b) }
+    fn adjacencies(self) -> HashMap<String, Vec<String>> {
+        let mut graph = HashMap::new();
+        for line in self.buf.lines().take(self.edges).map(|s| s.unwrap()) {
+            let edge: Vec<&str> = line.split_whitespace().collect();
+            let vertex = edge[0].to_string();
+            let neighbor = edge[1].to_string();
+            if !graph.contains_key(&vertex) {
+                graph.insert(vertex, vec![neighbor]);
+            } else {
+                graph.get_mut(&vertex).unwrap().push(neighbor);
+            }
+        }
+        graph
     }
 }
 
 fn main() {
     let filename = "graph.txt";
     let file = File::open(filename).unwrap();
-    let reader = BufReader::new(&file);
-    let graph = load_graph(reader);
-    for (v,es) in graph {
-        println!("node {}:", v.name);
-        for e in es {
-            println!("\t{} -> {}", e.a.name, e.b.name);
+    let graph = GraphReader::new(BufReader::new(&file));
+    let adj = graph.adjacencies();
+    for (vertex, neighbors) in adj {
+        println!("node {}:", vertex);
+        for n in neighbors {
+            println!("\t{} -> {}", vertex, n);
         }
     }
-}
-
-fn load_graph<T: BufRead>(mut reader: T) -> HashMap<Vertex, Vec<Edge>> {
-    let mut buffer = String::new();
-    reader.read_line(&mut buffer).unwrap();
-    let dim: Vec<usize> = buffer.split_whitespace().map(|s| s.parse().unwrap()).collect();
-    let mut graph = HashMap::new();
-    for line in reader.lines().take(dim[1]).map(|s| s.unwrap()) {
-        let edge: Vec<&str> = line.split_whitespace().collect();
-        let v = Vertex::new(edge[0]);
-        let e = Edge::new(edge[0], edge[1]);
-        if !graph.contains_key(&v) {
-            graph.insert(v, vec![e]);
-        } else {
-            graph.get_mut(&v).unwrap().push(e);
-        }
-    }
-    graph
 }
